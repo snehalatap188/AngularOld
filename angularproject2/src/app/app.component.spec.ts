2f1d3849c01780e36de86646feefda9d6a089e0a
snehalatap188 <77912677+snehalatap188@users.noreply.github.com>
2022-08-04 12:26:08 +0530

    Update app.component.spec.ts

--- angularproject2/src/app/app.component.spec.ts
+++ angularproject2/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    #expect(compiled.querySelector('.content span')?.textContent).toContain('angularproject2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularproject2 app is running!');
   });
 });
