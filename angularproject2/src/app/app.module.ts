64d930471954af0a020c55b5436f23277cb5d0fe
snehalatap188 <77912677+snehalatap188@users.noreply.github.com>
2022-08-04 12:49:27 +0530

    Update app.module.ts

--- angularproject2/src/app/app.module.ts
+++ angularproject2/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { HttpClientModule } from '@angular/common/http';
+#import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http'; 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule, HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
